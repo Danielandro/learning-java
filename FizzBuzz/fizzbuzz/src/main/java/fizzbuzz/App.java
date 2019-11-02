01f1b2109a0038cee27ba5910b60e67cceacd1fe
Danielandro <mtme13@hotmail.com>
2019-11-02 21:41:18 +0000

    Fizzbuzz: execute for 1-100

--- FizzBuzz/fizzbuzz/src/main/java/fizzbuzz/App.java
+++ FizzBuzz/fizzbuzz/src/main/java/fizzbuzz/App.java
@@ -9,9 +9,15 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
+    static Fizzbuzz fizzbuzz1 = new Fizzbuzz();
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(fizzbuzz1.converter(i));
+        }
     }
 }
