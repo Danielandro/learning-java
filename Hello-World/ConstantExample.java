aa189a69ac857def7c80a0bc83dde03472e3269c
Danielandro <mtme13@hotmail.com>
2019-11-03 20:35:28 +0000

    Add constants example

---
+++ Hello-World/ConstantExample.java
@@ -0,0 +1,8 @@
+public class ConstantExample {
+  public static void main(String[] args) {
+    // adding 'final' keyword, compiler treats as constant
+    // all caps for constants
+    final float PI = 3.14F;
+    System.out.println(PI);
+  }
+}
