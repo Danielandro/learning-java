285946e25b1b144c4adc675b460bac22dd8cca60
Danielandro <mtme13@hotmail.com>
2019-11-03 19:24:00 +0000

    Add arrays example

---
+++ Hello-World/ArrayExample.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class ArrayExample {
+  public static void main(String[] args) {
+    // create an array, max length = 5
+    int[] numbers = new int[5];
+
+    System.out.println(Arrays.toString(numbers));
+  }
+}
