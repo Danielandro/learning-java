fc14bc8b666574748dfcfd71cd216521da62a11e
Danielandro <mtme13@hotmail.com>
2019-11-03 15:49:48 +0000

    Fizzbuzz: refactor to use @Before

--- FizzBuzz/fizzbuzz/src/test/java/fizzbuzz/FizzbuzzTest.java
+++ FizzBuzz/fizzbuzz/src/test/java/fizzbuzz/FizzbuzzTest.java
@@ -3,29 +3,34 @@ package fizzbuzz;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class FizzbuzzTest {
+  private Fizzbuzz fizzbuzz;
+
+  @Before
+  public void setupFizzbuzz() {
+    fizzbuzz = new Fizzbuzz();
+  }
+
   @Test
   public void leavesNormalNumberAlone() {
-    Fizzbuzz fizzbuzz = new Fizzbuzz();
     assertEquals("1", fizzbuzz.converter(1));
     assertEquals("2", fizzbuzz.converter(2));
   }
 
   @Test
   public void returnsFizzIfMultipleOfThree() {
-    Fizzbuzz fizzbuzz = new Fizzbuzz();
     assertEquals("Fizz", fizzbuzz.converter(3));
   }
 
   @Test
   public void returnsBuzzIfMultipleOfFive() {
-    Fizzbuzz fizzbuzz = new Fizzbuzz();
     assertEquals("Buzz", fizzbuzz.converter(5));
   }
 
   @Test
   public void returnsFizzbuzzIfMultipleOfFifteen() {
-    Fizzbuzz fizzbuzz = new Fizzbuzz();
     assertEquals("Fizzbuzz", fizzbuzz.converter(15));
   }
 }
