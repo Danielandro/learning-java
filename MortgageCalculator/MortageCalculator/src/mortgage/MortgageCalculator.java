2a73aff2089245a48402820e121306e89060da91
Danielandro <mtme13@hotmail.com>
2019-11-04 18:05:04 +0000

    Refactor mortgage calculator

--- MortgageCalculator/MortageCalculator/src/mortgage/MortgageCalculator.java
+++ MortgageCalculator/MortageCalculator/src/mortgage/MortgageCalculator.java
@@ -5,49 +5,49 @@ import java.util.Scanner;
 
 public class MortgageCalculator {
     static int principal;
-    static double annualInterestRate;
+    static double annualRate;
     static int periodInYears;
     static String mortgageInDollars;
 
     public static void main(String[] args) throws Exception {
-
-        // int paymentPeriod;
-        // NumberFormat mortgage;
-        // System.out.println("Hey");
         getUserInput();
-        calculateMortgage();
+        getMortgage();
     }
 
     public static void getUserInput() {
-        // scan for user input
+        // scanner for user input
         Scanner scanner = new Scanner(System.in);
         System.out.print("Principal: ");
-        // get user input
         principal = scanner.nextInt();
 
-        // APR
         System.out.print("Annual Interest Rate: ");
-        annualInterestRate = scanner.nextDouble();
+        annualRate = scanner.nextDouble();
 
-        // Period
         System.out.print("Period (Years): ");
         periodInYears = scanner.nextInt();
 
         scanner.close();
-        // System.out.println("Principal: " + principal + "\nAnnual Interest Rate: " +
-        // annualInterestRate
-        // + "\nPeriod (Years): " + periodInYears);
     }
 
-    public static void calculateMortgage() {
-        double monthlyInterestRate = (annualInterestRate / 12) / 100;
+    public static void getMortgage() {
+        double monthlyRate = calculateRate();
         int periodInMonths = periodInYears * 12;
+        double mortgage = calculateMortgage(monthlyRate, periodInMonths);
+        printMortgage(mortgage);
+    }
 
-        double mortgage = principal * ((monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), periodInMonths)))
-                / (Math.pow((1 + monthlyInterestRate), periodInMonths) - 1));
-
+    public static void printMortgage(double mortgage) {
         mortgageInDollars = NumberFormat.getCurrencyInstance().format(mortgage);
-
         System.out.println("Mortgage: " + mortgageInDollars);
+    }
+
+    private static double calculateRate() {
+        return (annualRate / 12) / 100;
+    }
+
+    private static double calculateMortgage(double rate, int months) {
+        double numerator = rate * (Math.pow((1 + rate), months));
+        double denominator = Math.pow((1 + rate), months) - 1;
+        return principal * (numerator / denominator);
     }
 }
