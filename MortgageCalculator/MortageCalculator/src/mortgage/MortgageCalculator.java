d36ea2f87bc44b0e674ef4649d084b5a7a25f0cb
Danielandro <mtme13@hotmail.com>
2019-11-04 19:07:06 +0000

    Remove magic numbers from mortgage calculator

--- MortgageCalculator/MortageCalculator/src/mortgage/MortgageCalculator.java
+++ MortgageCalculator/MortageCalculator/src/mortgage/MortgageCalculator.java
@@ -4,14 +4,17 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MortgageCalculator {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
     static int principal;
-    static double annualRate;
-    static int periodInYears;
-    static String mortgageInDollars;
+    static float annualInterest;
+    static byte years;
+    static double mortgage;
 
     public static void main(String[] args) throws Exception {
         getUserInput();
         getMortgage();
+        printMortgage(mortgage);
     }
 
     public static void getUserInput() {
@@ -21,33 +24,32 @@ public class MortgageCalculator {
         principal = scanner.nextInt();
 
         System.out.print("Annual Interest Rate: ");
-        annualRate = scanner.nextDouble();
+        annualInterest = scanner.nextFloat();
 
         System.out.print("Period (Years): ");
-        periodInYears = scanner.nextInt();
+        years = scanner.nextByte();
 
         scanner.close();
     }
 
     public static void getMortgage() {
-        double monthlyRate = calculateRate();
-        int periodInMonths = periodInYears * 12;
-        double mortgage = calculateMortgage(monthlyRate, periodInMonths);
-        printMortgage(mortgage);
+        float monthlyInterest = calculateInterest();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+        mortgage = calculateMortgage(monthlyInterest, numberOfPayments);
     }
 
     public static void printMortgage(double mortgage) {
-        mortgageInDollars = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageInDollars);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 
-    private static double calculateRate() {
-        return (annualRate / 12) / 100;
+    private static float calculateInterest() {
+        return (annualInterest / MONTHS_IN_YEAR) / PERCENT;
     }
 
-    private static double calculateMortgage(double rate, int months) {
-        double numerator = rate * (Math.pow((1 + rate), months));
-        double denominator = Math.pow((1 + rate), months) - 1;
+    private static double calculateMortgage(double interest, int numOfPayments) {
+        double numerator = interest * (Math.pow((1 + interest), numOfPayments));
+        double denominator = Math.pow((1 + interest), numOfPayments) - 1;
         return principal * (numerator / denominator);
     }
 }
